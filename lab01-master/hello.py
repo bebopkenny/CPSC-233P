8a28aec58edb4bb5c862dd07c8bde24a10088c1d
bebopkenny <kennygarcia15@csu.fullerton.edu>
2024-11-11 01:53:21 -0800

    Simple hello world program

---
+++ lab01-master/hello.py
@@ -0,0 +1,9 @@
+# Name: Kenny Garcia
+# Date: 8/28/2024
+# File Purpose: Multiple hello functions
+
+def helloworld():
+    return "Hello World!"
+
+def helloname(name):
+    return "Hello " + name + "!" 
---
+++ lab01-master/main.py
@@ -0,0 +1,11 @@
+# Name: Kenny Garcia
+# Date: 8/24/2024
+# File Purpose: Lab01 main program driver program
+
+from hello import *
+
+print("*** TUFFY TITAN HELLO PROGRAM ***")
+
+print(helloworld())
+
+print(helloname("Tuffy"))
